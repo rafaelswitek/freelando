354e6c1da76275f3f843cbee904c4cbe4015d549
Rafael Gonçalves <rafaelswitek@gmail.com>
2024-01-24 12:36:38 -0300

    03. Tipografando a aplicação

---
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -0,0 +1,41 @@
+import { ThemeProvider } from '@emotion/react'
+
+const tema = {
+    cores: {
+        branco: '',
+        atencao: '',
+        focus: '',
+        primarias: {
+            a: '#5754ED',
+            b: '',
+            c: ''
+        },
+        secundarias: {
+            a: '#EBEAF9',
+            b: '',
+            c: ''
+        },
+        neutras: {
+            a: '#373737',
+            b: '',
+            c: '',
+            d: ''
+        },
+        dark: {
+            a: '',
+            b: ''
+        }
+    },
+    espacamentos: {
+       xs: '8px',
+       s : '16px',
+       l : '32px',
+    },
+    fontFamily: "'Montserrat', sans-serif"
+}
+
+export const ProvedorTema = ({ children }) => {
+    return <ThemeProvider theme={tema}>
+        { children }
+    </ThemeProvider>
+}
