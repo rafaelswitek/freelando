1acea4e067d29d7cbefb7b11f3061fa6cff11738
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-07-31 22:31:38 -0300

    03. Concluindo o fluxo

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom"
+import Concluido from "../paginas/cadastro/Concluido";
+import DadosPessoais from "../paginas/cadastro/DadosPessoais";
+import Interesses from "../paginas/cadastro/Interesses";
 import LayoutBaseCadastro from "../paginas/cadastro/LayoutBaseCadastro";
 import SelecaoCliente from "../paginas/cadastro/SelecaoCliente";
 import LayoutBase from "../paginas/LayoutBase";
-import PaginaInicial from "../paginas/PaginaInicial"
 
 export const router = createBrowserRouter([
     {
@@ -18,16 +20,16 @@ export const router = createBrowserRouter([
                         element: <SelecaoCliente />
                     },
                     {
-                        path: 'cliente',
-                        element: <h1>Interesesses</h1>
+                        path: 'interesses',
+                        element: <Interesses />
                     },
                     {
                         path: 'dados-pessoais',
-                        element: <h1>Dados pessoais</h1>
+                        element: <DadosPessoais />
                     },
                     {
                         path: 'concluido',
-                        element: <h1>Concluído</h1>
+                        element: <Concluido />
                     }
                 ]
             }
